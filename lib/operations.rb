e2e010b675da4022e1d92bdfda8683e2df30d517
Anthony Lepore <violacellonyc@gmail.com>
2020-07-27 20:48:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+
 def unsafe?(speed)
-
+if speed < 40 || speed > 60
+  return true
+else return false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
-end
-	
+ return (speed < 60 && speed > 40 ? false : true)
+  
+# return (speed < 60 && speed > 40 ? true : false)
+ # return (speed < 40 || speed > 60 ? true : false)
+#	speed < 40 || speed > 60 ? true : false
+# return speed > 60 || speed < 40 ? true : false
+  end
 
-
+  
