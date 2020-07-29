04f68a97cfe639ef9cf5ead35b553502e5d1f753
Tharon Weighill <tharonweighill@gmail.com>
2020-07-29 04:27:21 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,25 @@
+# declare a method called unsafe?
+# the method unsafe? takes in a argument of a speed
+
+#business logic
+
+# return true if >60
+# return true if <40
+# return false 40 >= || <= 60
+
+require 'pry'
+
 def unsafe?(speed)
+  if speed > 60 || speed < 40 
+    return true
+  else
+    return false
+  end  
 
 end
-
-
 
 def not_safe?(speed)
-	
-end
-	
-
-
+ speed > 60 || speed < 40 ? true : false
+ end
+ 
+ 
